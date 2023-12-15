6cb55c0b547f5a4068db5f97037073453d914397
serafindanyil <151615181+serafindanyil@users.noreply.github.com>
2023-12-15 18:25:05 +0200

    Update country.py
    Update code from lab-5

--- Lab-5/country.py
+++ Lab-5/country.py
@@ -8,20 +8,17 @@ class GovernmentType(Enum):
 
 
 class Country:
-    def __init__(self, name, capital, code,  population, area, GDP, government_type):
+    def __init__(self, name, capital, code, population, area, gdp, government_type):
         self.name = name
         self.capital = capital
         self.code = code
         self.population = population
         self.area = area
-        self.GDP = GDP
+        self.gdp = gdp
         self.government_type = government_type
 
-    #def calculate_population_density(self):
-    #   return self.population / self.area
-
     def __repr__(self):
-        return f"{self.name}, {self.capital}, {self.code}, {self.population}, {self.area}, {self.GDP}, {self.government_type}"
+        return f"{self.name}, {self.capital}, {self.code}, {self.population}, {self.area}, {self.gdp}, {self.government_type}"
 
 
 class Land:
@@ -32,8 +29,8 @@ class Land:
     def sort_population_density(self):
         return sorted(self.countries, key=lambda country: country.population / country.area, reverse=True)
 
-    def sort_countries_by_GDP(self):
-        return sorted(self.countries, key=lambda country: country.GDP, reverse=True)
+    def sort_countries_by_gdp(self):
+        return sorted(self.countries, key=lambda country: country.gdp, reverse=True)
 
     def choose_country(self, criteria_func):
         return max(self.countries, key=criteria_func)
@@ -44,11 +41,12 @@ class Land:
             print(f"Population density of {country.name}: {country.population / country.area} people per square kilometer")
 
     def get_top_gdp_countries(self):
-        top_gdp_countries = self.sort_countries_by_GDP()
+        top_gdp_countries = self.sort_countries_by_gdp()
         print(f"Top GDP countries in {self.name}")
         for country in top_gdp_countries:
-            print(f"{country.name}: {country.GDP} USD trillion")
+            print(f"{country.name}: {country.gdp} USD trillion")
 
     def get_longest_name(self):
         get_longest_name = self.choose_country(lambda country: len(country.name))
         print(f"Country in Europe with the longest name: {get_longest_name.name}")
+        
