2e635c57bb3a3ec9b91e08a4631fd1f47084df1b
serafindanyil <151615181+serafindanyil@users.noreply.github.com>
2023-12-15 18:25:31 +0200

    Update main.py
    Update code from lab-5

--- Lab-5/main.py
+++ Lab-5/main.py
@@ -1,30 +1,16 @@
 from country import Land, Country, GovernmentType
-import pytest
+
+
 def main():
-    Ukraine = Country("Ukraine", "Kyiv", "UA", 45000000, 603500, 1311.29, GovernmentType.DEMOCRACY)
-    Poland = Country("Poland", "Warsaw", "PL", 38000000, 312696, 614.86, GovernmentType.REPUBLIC)
-    France = Country("France", "Paris", "FR", 68000000, 551695, 2958, GovernmentType.REPUBLIC)
-    Ukraine2 = Country("Ukraine", "Kyiv", "UA", 45000000, 603500, 1311.29, GovernmentType.DEMOCRACY)
+    ukraine = Country("Ukraine", "Kyiv", "UA", 45000000, 603500, 1311.29, GovernmentType.DEMOCRACY)
+    poland = Country("Poland", "Warsaw", "PL", 38000000, 312696, 614.86, GovernmentType.REPUBLIC)
+    france = Country("France", "Paris", "FR", 68000000, 551695, 2958, GovernmentType.REPUBLIC)
 
-
-    Europe = Land("Europe", [Ukraine, Poland, France, Ukraine2])
-    Europe.get_top_population_density()
-    Europe.get_top_gdp_countries()
-    Europe.get_longest_name()
-
-    #for a in Europe:
-    #    if id in a:
-    print(id(Ukraine))
-    print(id(Ukraine2))
-    print(id(Poland))
-    print(id(France))
+    europe = Land("Europe", [ukraine, poland, france])
+    europe.get_top_population_density()
+    europe.get_top_gdp_countries()
+    europe.get_longest_name()
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
