c77c7d9a9a58921c32c9339c946b74a4fb20b6b4
serafindanyil <151615181+serafindanyil@users.noreply.github.com>
2023-12-16 10:35:22 +0200

    Update main.py
    Update code from lab-4

--- Lab-4/main.py
+++ Lab-4/main.py
@@ -1,14 +1,14 @@
-from artificial_tree import ArtificialTree
+from dishwasher import Dishwasher
 
 
 def main():
-    tree1 = ArtificialTree("Dongyi", 100, 2000, "plastic", True, "14.11.2020")
-    tree2 = ArtificialTree("Sunwing ", 120, 1000, "metalic", False, "13.12.2222")
-    tree3 = ArtificialTree("Nearly", 120, 3000, "wooden", True, "10.08.2024")
+    Dish1 = Dishwasher(2000, 10, 'Samsung', 40)
+    Dish2 = Dishwasher(10000, 5, 'LG', 60)
+    Dish3 = Dishwasher(12000, 24, 'Toshiba', 15)
 
-    print(tree1)
-    print(tree2)
-    print(tree3)
+    print(Dish1)
+    print(Dish2)
+    print(Dish3)
 
 
 if __name__ == "__main__":
